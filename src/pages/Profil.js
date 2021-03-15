3fa437ff760039ccae458abc4708f2e6a9603c78
ludovic_gendre <ludovic.gendre@g-mail.fr>
2021-03-15 16:46:02 +0100

    feature: update view profile and added btn response gameplay

--- src/pages/Profil.js
+++ src/pages/Profil.js
@@ -3,6 +3,7 @@ import { Container } from 'react-bootstrap';
 import { Link } from "react-router-dom";
 import logo from '../assets/20944771.jpg'
 import player from '../components/player'
+import { disconnectUser } from "../components/userEffects";
 
 
 
@@ -13,34 +14,65 @@ const Profil = () => {
   if (error) return "An error has occurred: " + error.message;
   return (
     <>
-      <section style={{ marginTop: '5%', marginBottom: '15%' }} className='py-8 py-md-11 border-bottom'>
-        <Container>
-          <img className='mx-auto d-block rounded-circle' style={{height:'200px',width:'20%'}} src={data?.player?.avatar} alt='avatar player'></img>
-          <h2 className='text-center'>We are happy to see you again, {data?.player.name}</h2>
-          <br></br>
-          <div className='row'>
-            <br/>
-            <div className='col-12 col-md-4'>
-              <h3>Total Game Played</h3>
-              <p className='text-muted mb-6 mb-md-0'>
-                You currently have {data?.player.nb_played_games} games played
-              </p>
-            </div>
-            <div className='col-12 col-md-4'>
-            <h3>Total Game Played</h3>
-              <p className='text-muted mb-6 mb-md-0'>
-                You currently have {data?.player.nb_played_games} games played
-              </p>
-            </div>
-            <div className='col-12 col-md-4'>
-            <h3>Total Game Played</h3>
-              <p className='text-muted mb-6 mb-md-0'>
-                You currently have {data?.player.nb_played_games} games played
-              </p>
+    <header className='bg-dark pt-9 pb-11 d-none d-md-block' style={{paddingBottom:'10%'}}>
+      <Container>
+        <div className='row align-items-center'>
+          <div className='col'>
+            <h1 className='font-weight-bold text-white mb-2'>
+              Account Settings
+            </h1>
+            <p className='font-size-lg text-white mb-0'>
+            Settings for {data?.player.name}
+            </p>
+          </div>
+          <div className='col-auto'>
+            <button className='btn btn-sm text-white btn-secondary' 
+             to="/auth"
+            onClick={() => disconnectUser()} >
+                Log Out
+            </button>
+          </div>
+        </div>
+      </Container>
+    </header>
+    <main className='pb-8 pb-md-11 mt-md-n6' style={{marginTop:'-5%'}}>
+      <Container>
+        <div className='row'>
+          <div className='col-12 col-md-3'>
+          </div>
+          <div className='col-12 col-md-9'>
+            <div className='card card-bleed shadow-light-lg mb-6'>
+              <div className='card-header'>
+                <h4>Basic Information</h4>
+              </div>
+              <div className='card-body'>
+                <div className='row'>
+                  <div className='col-12 col-md-6'>
+                    <div className='form-group'>
+                      <label>Name</label>
+                      <p>{data?.player.name}</p>
+                    </div>
+                  </div>
+                  <div className='col-12 col-md-6'>
+                    <div className='form-group'>
+                      <label>Total Game</label>
+                      <p>{data?.player.nb_played_games}</p>
+                    </div>
+                  </div>
+                  <div className='col-12'>
+                  <div className='form-group'>
+                    <label>Avatar</label>
+                    <img src={data?.player?.avatar} alt='icon avatar player'
+                  className='mx-auto d-block' style={{height:'150px'}} />
+                  </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
-        </Container>
-      </section>
+        </div>
+      </Container>
+    </main>
     </>
   )
 }
